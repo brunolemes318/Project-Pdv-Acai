c42f91a1315375ce9f5a0909822289e4f4f32302
brunolemes <brunolemes318@gmail.com>
2021-10-26 23:34:44 -0300

    adicionado componentes e temas

---
+++ lib/pages/components/components_pagedefault.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import '../../themes/theme_default.dart';
+
+class ComponentsPageDefault {
+  static AppBar menuAppBar = AppBar();
+
+  ComponentsPageDefault();
+
+  static AppBar get menuDefaultOfApp => menuAppBar;
+}
---
+++ lib/pages/default_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'components/components_pagedefault.dart';
+
+class PageDefault extends StatelessWidget {
+  late Widget menu;
+  late SvgPicture logo;
+  late Drawer hamburguerMenu;
+
+  PageDefault({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: ComponentsPageDefault.menuDefaultOfApp,
+    );
+  }
+}
