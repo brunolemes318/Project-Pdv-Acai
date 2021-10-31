887478410873651dc31f5f9c054b9ab83657c9e9
brunolemes <brunolemes318@gmail.com>
2021-10-31 18:16:25 -0300

    adicionado menu para o desktop

--- lib/components/components_default.dart
+++ lib/components/components_default.dart
@@ -4,7 +4,16 @@ import '../../themes/theme_default.dart';
 abstract class ComponentsDefault {
   AppBar menuOfApp = AppBar();
   Drawer menuHamburguerOfApp = Drawer();
+  Image logo = Image.asset('');
   ComponentsDefault();
 
-  AppBar get menu => menuOfApp;
+  AppBar get menu {
+    return menuOfApp = AppBar(
+      title: logo,
+    );
+  }
+
+  Widget get itensOfMenu{
+    return Row();
+  }
 }
--- lib/pages/desktop/home_page_desktop.dart
+++ lib/pages/desktop/home_page_desktop.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:pdv/pages/default_page.dart';
 import './../../components/components_desktop.dart';
+
 class HomePageDeskop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PageDefault(appBar: ComponentsDesktop().menuOfApp,);
+    return PageDefault(
+      appBar: ComponentsDesktop().menuOfApp,
+    );
   }
 }
